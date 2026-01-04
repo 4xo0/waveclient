5d3967bf76799149fca0e91b0024b5db14c521a3
4xo0 <szymanskifranek23@gmail.com>
2026-01-04 08:16:47 -0800

    Add preview configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 42069
+  },
+  preview: {
+    port: 4443,
+    host: '0.0.0.0',
+    allowedHosts: ['evades-pkez.onrender.com']
   }
 });
